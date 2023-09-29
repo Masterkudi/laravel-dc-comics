08d40a6301ee3e0ea04a88e262ed516a6294d3ee
masterkudi <cudini.andrea@gmail.com>
2023-09-29 19:50:57 +0200

    creo controller, model, migration e seeder

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comic;
+
+class ComicController extends Controller
+{
+    public function index()
+    {
+        $comics = Comic::all();
+
+
+
+        return view('homepage', [
+            "comics" => $comics
+        ]); 
+    }
+}
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,24 @@
-@extends("layouts.public")
+@extends('layouts.public')
 
-@section("content")
-    <h1>Hello Folks!</h1>
+@section('content')
+    <div class="container card-container">
+        <div class="row row-cols-3 g-5 justify-content-center">
+
+            @foreach ($comics as $comic)
+                <div class="col">
+                    <div class="card p-3 bg-white">
+                        <div class="card-body">
+                            <img src="{{ $comic['thumb'] }}" alt="comic-img">
+                            <h3 class="card-title">{{ $comic->title }}</h3>
+                            <h4 class="card-subtitle mb-2 text-body-secondary">{{ $comic->price }}</h4>
+                            <p class="card-text">series: {{ $comic->series }}</p>
+                            <p class="card-text">date: {{ $comic->sale_date }}</p>
+                            <p class="card-text">comic type: <strong>{{ $comic->type }}</strong></p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
 @endsection
